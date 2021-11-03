0b4c83c2a35924254869c6f125e65c7fd1b9acd6
Cherron93 <colinherron93@gmail.com>
2021-11-03 06:57:51 -0400

    final commit

--- script.js
+++ script.js
@@ -1,78 +1,38 @@
-// const prev = document.getElementById('prev');
-// const next = document.getElementById('next');
-// const steps = document.querySelectorAll('steps')
-// const step1 = document.getElementById('step1');
-// const step2 = document.getElementById('step2');
-// const step3 = document.getElementById('step3');
-// const step4 = document.getElementById('step4');
-
-// // next.addEventListener('click', function () {
-// //     step2.classList.remove('inactive');
-// // })
-
-// // next.addEventListener('click', function () {
-// //     step3.classList.remove('inactive');
-// // })
-
-// // next.addEventListener('click', function () {
-// //     step4.classList.remove('inactive');
-// // })
-
-// for (let i = 0; i <5; i++){
-//     next.addEventListener('click', function () {
-//         steps[i].classList.remove('inactive');
-//     })
-// }
-
-const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const boxes = document.querySelectorAll('.box')
 
-let currentActive = 1
-
-next.addEventListener('click', () => {
-    currentActive++
-
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-    }
-
-    console.log(currentActive)
-
-    update()
-
-})
+let currentActive = 0;
 
 prev.addEventListener('click', () => {
-    currentActive--
-
-    if(currentActive < 1) {
-        currentActive = 1
+    if (currentActive > 0) {
+        currentActive --
+        console.log(currentActive)
+        let active = boxes[currentActive]
+        let priorActive = boxes[currentActive + 1]
+        active.classList.add('active')
+        priorActive.classList.remove('active')
+        buttonDisable()
     }
-
-    console.log(currentActive)
-    
-    update()
-
 })
 
-function update() {
-    circles.forEach ((circle, idx) => {
-        if (idx < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
+next.addEventListener('click', () => {
+    if (currentActive < 5) {
+        currentActive ++
+        console.log(currentActive)
+        let active = boxes[currentActive]
+        let prevActive = boxes[currentActive - 1]
+        active.classList.add('active')
+        prevActive.classList.remove('active')
+        buttonDisable()
 
-    const actives = document.querySelectorAll('.active')
+    } 
+})
 
-    progress.style.width = ((actives.length - 1)/ (circles.length - 1)) * 100 + '%'
-
-    if (currentActive === 1) {
+function buttonDisable() {
+    if (currentActive === 0) {
         prev.disabled = true;
-    } else if (currentActive === circles.length) {
+    } else if (currentActive === 5) {
         next.disabled = true;
     } else {
         prev.disabled = false
